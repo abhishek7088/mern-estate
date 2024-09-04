9d74d362e52c4e73e73572d286e6d965a278c939
abhishek7088 <abhi7088721935@gmail.com>
2024-09-04 18:03:43 +0530

    Header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,10 @@ import { FaSearch } from 'react-icons/fa';
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto py-4'> {/* Added py-4 to add equal padding on top and bottom */}
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-          <span className='text-slate-500'>Airbnb</span>
+          <span className='text-slate-400'>Air</span>
+          <span className='text-slate-500'>bnb</span>
         </h1>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
           <input
@@ -19,16 +20,14 @@ export default function Header() {
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
-          <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li> 
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
           </Link>
           <Link to='/about'>
-          <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
           </Link>
-          
-          <Link to='/sign-in '>
-           <li className='text-slate-700 hover:underline'>Sign In</li>
-           </Link>
-         
+          <Link to='/sign-in'>
+            <li className='text-slate-700 hover:underline'>Sign In</li>
+          </Link>
         </ul>
       </div>
     </header>
