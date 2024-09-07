a7993f5605b28bc4e5aed72b25b37b7dcdccfba2
abhishek7088 <abhi7088721935@gmail.com>
2024-09-07 13:27:38 +0530

    created-signup-api-route

--- APi/index.js
+++ APi/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() =>{ console.log('Connected to MongoDB')})
@@ -11,7 +12,8 @@ mongoose.connect(process.env.MONGO).then(() =>{ console.log('Connected to MongoD
 const app = express();
 
 app.listen(3000, () => {
-  console.log('listening on the port 3000!');
+  console.log('listening on the port 3000');
 });
-
+app.use(express.json());
 app.use('/APi/user',userRouter);
+app.use('/APi/auth',authRouter);
