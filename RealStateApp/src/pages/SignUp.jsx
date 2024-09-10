376a6ead76a2e20c9fc3c3c4fd0514d52787415e
abhishek7088 <abhi7088721935@gmail.com>
2024-09-10 18:18:11 +0530

    Sign Up page UI created

--- RealStateApp/src/pages/SignUp.jsx
+++ RealStateApp/src/pages/SignUp.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>
-      <h1>Sign Up Page</h1>
+    <div className='p-4 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-6'>Sign Up </h1>
+      <form className='flex flex-col gap-4'>
+         < input type="text" placeholder='username' className='border p-3 rounded-lg ' id="username"/>
+         < input type="text" placeholder='email' className='border p-3 rounded-lg ' id="email"/>
+         < input type="text" placeholder='password' className='border p-3 rounded-lg ' id="password"/>
+         <button  className=' bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-80'>Sign Up</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account ?</p>
+        <Link to={"/sign-in"}></Link>
+        <span className='text-blue-600'>Sign in</span>
+      </div>
     </div>
   )
 }
